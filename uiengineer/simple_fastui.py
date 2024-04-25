20aa5f485207349ef12815fd6d372c3228747e36
Shroominic <db2002dominic@gmail.com>
2024-04-25 14:58:57 +0200

    🔧 Refactor simple_fastui.py with OOP principles, introducing abstract base Component class and updating component classes

--- uiengineer/simple_fastui.py
+++ uiengineer/simple_fastui.py
@@ -8,30 +8,47 @@ because there are many cross references between the components
 but we can maybe solve this differently feel free to change this if you have a better idea
 """
 
+from abc import ABC, abstractmethod
 from typing import Union
 from pydantic import BaseModel, Field
-from fastui.components import AnyComponent as FastUIAnyComponent
+from fastui.components import AnyComponent as FastUIComponent
 from fastui import components as c, events as e
 
 
-class Text(BaseModel):
+class Component(BaseModel, ABC):
+    class_name: str = Field(description="Optional bootstrap class names for styling.")
+
+    @abstractmethod
+    def to_fastui(self, **kwargs: str) -> FastUIComponent: ...
+
+
+class Text(Component):
     text_content: str = Field(description="Content of simple text component.")
 
+    def to_fastui(self, **kwargs: str) -> FastUIComponent:
+        return c.Div(
+            components=[c.Text(text=self.text_content)],
+            class_name=self.class_name,
+        )
 
-class StyledComponent(BaseModel):
-    bootstrap_class_names: str = Field(
-        description="Optional bootstrap class names for styling."
-    )
 
-
-class Button(StyledComponent):
+class Button(Component):
     text_content: str = Field(description="Button text.")
     on_click_action: str = Field(
         description="Description of what happens to the app when the button is clicked."
     )
 
+    def to_fastui(self, **kwargs: str) -> FastUIComponent:
+        return c.Button(
+            text=self.text_content,
+            class_name=self.class_name,
+            on_click=e.GoToEvent(
+                url=f"/{kwargs.get('app_name')}?action={self.on_click_action}"
+            ),
+        )
 
-class InputField(StyledComponent):
+
+class InputField(Component):
     """Form to input data (already includes submit button)"""
 
     label: str = Field(description="Label for the input field.")
@@ -44,8 +61,33 @@ class InputField(StyledComponent):
     )
     submit_button_label: str = Field(description="Label for the submit button.")
 
+    def to_fastui(self, **kwargs: str) -> FastUIComponent:
+        return c.Form(
+            loading=[c.Spinner()],
+            submit_url=f"/api/{kwargs.get('app_name')}",
+            submit_trigger=e.PageEvent(
+                name=self.on_submit_action,
+                next_event=e.GoToEvent(
+                    url=f"/{kwargs.get('app_name')}?action={self.on_submit_action}"
+                ),
+            ),
+            form_fields=[
+                c.FormFieldInput(
+                    title="Update instructions",
+                    name="update_instructions",
+                    initial=self.on_submit_action,
+                    class_name="d-none",
+                ),
+                c.FormFieldInput(
+                    name=self.label,
+                    title=self.placeholder,
+                    class_name=self.class_name,
+                ),
+            ],
+        )
 
-class Link(StyledComponent):
+
+class Link(Component):
     text_content: str = Field(description="Link text.")
     on_click_action: str = Field(
         description=(
@@ -54,79 +96,37 @@ class Link(StyledComponent):
         )
     )
 
+    def to_fastui(self, **kwargs: str) -> FastUIComponent:
+        return c.Link(
+            components=[c.Text(text=self.text_content)],
+            class_name=self.class_name,
+            on_click=e.GoToEvent(
+                url=f"/{kwargs.get('app_name')}?action={self.on_click_action}"
+            ),
+        )
 
-class Container(StyledComponent):
+
+class Container(Component):
+    """
+    A container for other components. (div)
+    For a vertical list of components, add the class_name "flex flex-col",
+    a horizontal list: "flex flex-row", and so on...
+    """
+
     components: list["AnyComponent"]
+
+    def to_fastui(self, **kwargs: str) -> FastUIComponent:
+        return c.Div(
+            components=patch(self.components, **kwargs),
+            class_name=self.class_name,
+        )
 
 
 AnyComponent = Union[Button, Text, InputField, Link, Container]
 
 
-def render(components: list[AnyComponent], app_name: str) -> list[FastUIAnyComponent]:
+def patch(components: list[AnyComponent], app_name: str) -> list[FastUIComponent]:
     """
     Convert a list of simplified components into actual fastui components.
     """
-    fastui_components: list[FastUIAnyComponent] = []
-    for component in components:
-        if isinstance(component, Button):
-            fastui_components.append(
-                c.Button(
-                    text=component.text_content,
-                    class_name=component.bootstrap_class_names,
-                    on_click=e.GoToEvent(
-                        url=f"/{app_name}?action={component.on_click_action}"
-                    ),
-                )
-            )
-
-        elif isinstance(component, Text):
-            fastui_components.append(c.Text(text=component.text_content))
-
-        elif isinstance(component, InputField):
-            fastui_components.append(
-                c.Form(
-                    loading=[c.Spinner()],
-                    submit_url=f"/api/{app_name}",
-                    submit_trigger=e.PageEvent(
-                        name=component.on_submit_action,
-                        next_event=e.GoToEvent(
-                            url=f"/{app_name}?action={component.on_submit_action}"
-                        ),
-                    ),
-                    form_fields=[
-                        c.FormFieldInput(
-                            title="Update instructions",
-                            name="update_instructions",
-                            initial=component.on_submit_action,
-                            class_name="d-none",
-                        ),
-                        c.FormFieldInput(
-                            name=component.label,
-                            title=component.placeholder,
-                            class_name=component.bootstrap_class_names,
-                        ),
-                    ],
-                )
-            )
-
-        elif isinstance(component, Link):
-            fastui_components.append(
-                c.Link(
-                    components=[c.Text(text=component.text_content)],
-                    class_name=component.bootstrap_class_names,
-                    on_click=e.GoToEvent(
-                        url=f"/{app_name}?action={component.on_click_action}"
-                    ),
-                )
-            )
-
-        elif isinstance(component, Container):
-            fastui_components.append(
-                c.Div(
-                    components=render(component.components, app_name=app_name),
-                    class_name=component.bootstrap_class_names,
-                )
-            )
-        else:
-            raise ValueError(f"Unknown component type: {component}")
-    return fastui_components
+    return [c.to_fastui(app_name=app_name) for c in components]
